af3603f7bf004d4e74cfa0f1e11e5f69e80be422
PANYOR <m2@panyor.com>
2020-08-25 18:49:56 +0100

    finishing 1.0.0

--- DirMaker/MainWindow.xaml.cs
+++ DirMaker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using DirMaker.model;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -31,6 +32,8 @@ namespace DirMaker
         public MainWindow()
         {
             InitializeComponent();
+            btn_SelectRoot.IsEnabled = false;
+            btn_goProcedure.IsEnabled = false;
         }
 
         private void btn_SelectFile_Click(object sender, RoutedEventArgs e)
@@ -41,12 +44,23 @@ namespace DirMaker
             {
                 this.filepath = openFileDialog.FileName;
                 lbl_StatusMessage.Content = "Selected input file: " + openFileDialog.SafeFileName;
+                btn_SelectRoot.IsEnabled = true;
 
             }
         }
 
         private void btn_goProcedure_Click(object sender, RoutedEventArgs e)
         {
+            FolderMaker folderMaker = new FolderMaker(filepath, rootFolderPath);
+            if (folderMaker.goProcedure())
+            {
+                lbl_StatusMessage.Content = "Operation was successful!";
+                lbl_StatusMessage.Foreground = Brushes.Green;
+            } else
+            {
+                lbl_StatusMessage.Content = "Operation has failed!";
+                lbl_StatusMessage.Foreground = Brushes.Red;
+            }
 
         }
 
@@ -57,8 +71,9 @@ namespace DirMaker
             if (dialog.SelectedPath != "")
             {
                 rootFolderPath = dialog.SelectedPath;
+                lbl_StatusMessage.Content += "\nSelected output folder: " + dialog.SelectedPath;
+                btn_goProcedure.IsEnabled = true;
             }
-            
         }
     }
 }
---
+++ DirMaker/model/FolderMaker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirMaker.model
+{
+
+    class FolderMaker
+    {
+        private string inputFilePath;
+        private string outputFolderPath;
+
+        public FolderMaker(string inputFilePath, string outputFolderPath)
+        {
+            this.inputFilePath = inputFilePath;
+            this.outputFolderPath = outputFolderPath;
+        }
+
+        public bool goProcedure() {
+
+            string alltext = null;
+            try
+            {
+                alltext = File.ReadAllText(inputFilePath);
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+                
+            string[] folders = alltext.Split('\n');
+
+            foreach (string item in folders)
+            {
+                try { 
+                    Directory.CreateDirectory(outputFolderPath + "\\" + item);
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
